d931ee8b648ba1b47c23a571210c965182fa5282
William Daniel da Silva Kuhs <wdarchenemy@gmail.com>
2022-02-17 18:33:17 -0300

    Initial Commit

--- Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Controller/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var storyBrain: StoryBrain!
+    
     @IBOutlet weak var storyLabel: UILabel!
     @IBOutlet weak var choice1Button: UIButton!
     @IBOutlet weak var choice2Button: UIButton!
@@ -17,8 +19,22 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        storyBrain = StoryBrain()
+        updateUI(with: storyBrain.currentStory)
     }
 
-
+    func updateUI(with story: Story) {
+        storyLabel.text = story.title
+        choice1Button.setTitle(story.choice1, for: .normal)
+        choice2Button.setTitle(story.choice2, for: .normal)
+    }
+    
+    @IBAction func choiceMad(_ sender: UIButton){
+        guard let buttonId = sender.restorationIdentifier else { return }
+        
+        storyBrain.nextStory(choice: buttonId)
+        
+        updateUI(with: storyBrain.currentStory)
+    }
 }
 
